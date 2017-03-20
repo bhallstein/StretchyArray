f37c7fe48b6e7b295b8497ccde272147ac9f8608
Ben <benhall@brandwatch.com>
2017-03-20 09:20:57 +0000

    test.cpp: actual tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,21 @@
 #include "../StretchyArray_swapRemove.h"
 #include <cstdio>
+#include <cassert>
+
+#define p_assert(x) do {             \
+		printf("%60s", #x);    \
+		assert(x);                   \
+		printf(" - PASS :)\n");      \
+	} while (false)
+
+#define p_header(s) do {                               \
+		for (int i=0; s[i] != '\0'; ++i) printf("*");  \
+		printf("********\n");                          \
+		printf("**  %s  **\n", s);                     \
+		for (int i=0; s[i] != '\0'; ++i) printf("*");  \
+		printf("********\n");                          \
+	} while (false)
+
 
 int main() {
 	StretchyArray<float, int> x;
@@ -8,11 +24,38 @@ int main() {
 	x.push(8);
 	x.push(9);
 
-	printf("%.0f,%.0f,%.0f,%.0f (%d)\n", x[0], x[1], x[2], x[3], x.get_n());
+	p_header("Initialised array has expected size");
+	p_assert(x.get_n() == 4);
+	p_assert(x.get_capacity() == 4);
+
+	p_header("Initialised array has expected values");
+	p_assert(x[0] == 6);
+	p_assert(x[1] == 7);
+	p_assert(x[2] == 8);
+	p_assert(x[3] == 9);
+
+	p_header("Removal returns previous index of last item");
 	printf("Removing: 1\n");
 	int ind_of_swapped_item = StretchyArray_swapRemove(x, 1);
-	printf("Item now swapped into position 1 previously had index: %d\n", ind_of_swapped_item);
-	printf("%.0f,%.0f,%.0f (%d/%d)\n", x[0], x[1], x[2], x.get_n(), x.get_capacity());
+	p_assert(ind_of_swapped_item == 3);
+
+	p_header("After removal, expected size");
+	p_assert(x.get_n() == 3);
+	p_assert(x.get_capacity() == 4);
+
+	p_header("After removal, expected values");
+	p_assert(x[0] == 6);
+	p_assert(x[1] == 9);
+	p_assert(x[2] == 8);
+
+	p_header("Compaction");
+	StretchyArray_swapRemove(x, 2);
+	p_assert(x.get_n() == 2);
+	p_assert(x.get_capacity() == 4);
+
+	StretchyArray_swapRemove(x, 1);
+	p_assert(x.get_n() == 1);
+	p_assert(x.get_capacity() == 2);
 
 	return 0;
 }
