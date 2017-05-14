410c5b002692a2c67e4b496f16e6ca44a8a42153
Ben <ben@Fratercula.local>
2017-05-14 13:50:47 +0100

    Tweak test/run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,22 @@
 set -e
 
+compile_and_run() {
+  if [ $# -ne 2 ]; then
+		echo "?"
+	else
+		f=$1
+		f_out="$f.out"
+		compile_cmd=$2
+		$compile_cmd $f -o $f_out
+		"./$f_out"
+		rm "$f_out"
+	fi
+}
+
 for f in *.cpp; do
-	clang++ $f -std=c++11 -o out
-	./out
-	rm out
+	compile_and_run $f "clang++ -std=c++11"
 done
 
 for f in *.c; do
-	clang $f -o out
-	./out
-	rm out
+	compile_and_run $f clang
 done
